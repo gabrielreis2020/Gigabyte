a2882c995acf5728c06825005c962280a0e17390
Gabriel Marques <gabrielreismarques123@gmail.com>
2020-05-06 15:12:21 -0300

    Trabalho Tabela de preços 1

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    Tabela = [
+        ['Refrigerante', 4.50],
+        ['Pizza', 2.50],
+        ['Suco', 24.90],
+        ['Salgado',	5.50],
+        ['Lanche', 18.50   ]
+    ]
+    return render_template(
+        'index.html',
+        titulo='Tabela',
+        Tabela=Tabela
+
+    )
+
+if __name__ == '__main__':
+    app.run()
